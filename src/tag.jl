b37427ee06e90c83ba6fe21e6e417d0ffaa1d4d6
Zenna Tavares <zennatavares@gmail.com>
2022-02-06 18:38:31 -0500

    format

--- src/tag.jl
+++ src/tag.jl
@@ -12,4 +12,5 @@ Trait{T} = AndTrait{Union{ATrait{T}}}
 conjointraits(::Type{AndTrait{T1}}, ::Type{AndTrait{T2}}) where {T1, T2} =
   AndTrait{Union{T1, T2}}
 
-conjointraits(t1, t2, ts...) = conjointraits(conjointraits(t1, t2), ts...)
+  conjointraits(t1::Type{<:AndTrait}, t2::Type{<:AndTrait}, ts...) =
+  conjointraits(conjointraits(t1, t2), ts...)
